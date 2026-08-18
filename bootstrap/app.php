2e8417743e101a008be2cf45e18cf3dbc19d1717
Ady Budisantika <budaya2017@gmail.com>
2026-08-18 14:20:02 +0800

    optimize app in boostrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -23,3 +23,14 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
+// Arahkan path public ke public_html jika di hosting production (cPanel)
+if ($customPublicPath = env('APP_PUBLIC_PATH')) {
+    $app->usePublicPath(str_starts_with($customPublicPath, '/') ? $customPublicPath : dirname(__DIR__).'/'.$customPublicPath);
+} elseif (is_dir(dirname(__DIR__).'/../public_html')) {
+    $app->usePublicPath(dirname(__DIR__).'/../public_html');
+} elseif (is_dir(dirname(__DIR__).'/public_html')) {
+    $app->usePublicPath(dirname(__DIR__).'/public_html');
+}
+
+return $app;
